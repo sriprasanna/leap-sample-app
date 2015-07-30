832e86be9d8c38c828d028345209800391090423
claucece <soficeli0@gmail.com>
2015-07-30 14:38:20 -0500

    helpers

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-  end
+    assert_select "a[href=?]", signup_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end 
+
 end
