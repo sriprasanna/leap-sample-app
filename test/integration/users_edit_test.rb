129c4a8482b4d96a61c5c0d5cbe5161a9a1665bb
claucece <soficeli0@gmail.com>
2015-08-06 12:45:52 -0500

    Some tests and assings

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -20,7 +20,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
     log_in_as(@user)
-    assert_redirected_to edit_user_path(@user)
+    get signup_path
     email = "foo@bar.com"
     patch user_path(@user), user: { name:  name,
                                     email: email,
